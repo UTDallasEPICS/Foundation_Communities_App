70a354795821c02493cfe8925149e9c892ad9a16
Nomaan Khan <43221868+nomaanakhtar@users.noreply.github.com>
2018-12-09 23:34:51 -0600

    Update environment.ts

--- empDashboard/empDashboard/src/environments/environment.ts
+++ empDashboard/empDashboard/src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCB2jqhzpcr-PvOvYxpBbs4Ug0Nye1LDDE",
-    authDomain: "ecom3-6dcc9.firebaseapp.com",
-    databaseURL: "https://ecom3-6dcc9.firebaseio.com",
-    projectId: "ecom3-6dcc9",
-    storageBucket: "ecom3-6dcc9.appspot.com",
-    messagingSenderId: "546968516703"     
+    
   }
 };
