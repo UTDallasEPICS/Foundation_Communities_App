1fd7acd15434f30b97fa81450cc4341b55228853
Jonathan Harty <jonathanharty99@gmail.com>
2020-01-12 00:04:46 -0600

    create list of needed items

--- items.js
+++ items.js
@@ -1,24 +1,147 @@
+import localization from './localizations';
+
 const items = {
   isCompleted: false,
   list: [
     {
-      name: 'item 1',
+      name: localization.basicInfo,
+      isHeader: true,
+    },
+    {
+      name: localization.ssCard,
+      isHeader: false,
       isChecked: false,
     },
     {
-      name: 'item 2',
+      name: localization.idCard,
+      isHeader: false,
       isChecked: false,
     },
     {
-      name: 'item 3',
+      name: localization.bankInfo,
+      isHeader: false,
       isChecked: false,
     },
     {
-      name: 'item 4',
+      name: localization.prevTaxReturn,
+      isHeader: false,
       isChecked: false,
     },
     {
-      name: 'item 5',
+      name: localization.docsReceived,
+      isHeader: true,
+    },
+    {
+      name: localization.w2,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1099,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1095A,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1095B,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1095C,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.exemption,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1098T,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1098E,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1099SSA,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1098,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.realEstateTax,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.w2G,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.personalRecs,
+      isHeader: true,
+    },
+    {
+      name: localization.careExpenses,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.educationExpenses,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.medicalExpenses,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.charitableContr,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.retirementContr,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.energyReceipt,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.selfEmployed,
+      isHeader: true,
+    },
+    {
+      name: localization.businessExpenses,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.businessIncome,
+      isHeader: false,
+      isChecked: false,
+    },
+    {
+      name: localization.form1099Misc,
+      isHeader: false,
       isChecked: false,
     },
   ],
