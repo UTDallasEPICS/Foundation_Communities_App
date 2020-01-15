9a3d48d2f8d06de42ac9f313449e49332d57a5bd
Jonathan Harty <jonathanharty99@gmail.com>
2020-01-15 11:39:23 -0600

    Fix firebase operations

--- screens/admin.js
+++ screens/admin.js
@@ -5,7 +5,7 @@ import {
 import { Input } from 'react-native-elements';
 import Touchable from 'react-native-platform-touchable';
 import firebase from 'react-native-firebase';
-import storage from '@react-native-firebase/storage';
+// import storage from '@react-native-firebase/storage';
 import Geocode from 'react-geocode';
 import Snackbar from 'react-native-snackbar';
 import styles from '../styles/styles';
@@ -40,9 +40,7 @@ const myStyles = StyleSheet.create({
   submitButtonText: {
     color: 'white',
   },
-  editTools: {
-
-  },
+  editTools: {},
 });
 
 export default class admin extends React.Component {
@@ -62,7 +60,7 @@ export default class admin extends React.Component {
       title: '',
       description: '',
     },
-  }
+  };
 
   static navigationOptions = {
     headerTitle: () => <Text style={styles.headertitle}>Admin</Text>,
@@ -74,248 +72,305 @@ export default class admin extends React.Component {
   };
 
   updateLocation() {
-    Geocode.fromAddress(this.state.newLoc.description).then((res) => {
-      const { lat, lng } = res.results[0].geometry.location;
-      const markerRef = firebase.database().ref('locationMap/markers');
+    Geocode.fromAddress(this.state.newLoc.description)
+      .then((res) => {
+        const { lat, lng } = res.results[0].geometry.location;
+        const markerRef = firebase.database().ref('locationMap/markers');
 
-      const newLocations = [...this.state.locations];
-      newLocations[this.state.current.index] = {
-        coordinate: { latitude: lat, longitude: lng },
-        description: this.state.newLoc.description,
-        title: this.state.newLoc.title,
-      };
+        const newLocations = [...this.state.locations];
+        newLocations[this.state.current.index] = {
+          coordinate: { latitude: lat, longitude: lng },
+          description: this.state.newLoc.description,
+          title: this.state.newLoc.title,
+        };
 
-      this.setState({ locations: newLocations });
+        this.setState({ locations: newLocations });
 
-      markerRef.set(newLocations).then(() => {
+        markerRef
+          .set(newLocations)
+          .then(() => {
+            Snackbar.show({
+              title: 'Location Updated',
+              duration: Snackbar.LENGTH_SHORT,
+            });
+
+            this.setState({
+              current: {
+                title: '',
+                index: 0,
+              },
+              locations: newLocations,
+            });
+          })
+          .catch((err) => {
+            Snackbar.show({
+              title: 'Error in Updating Location',
+              duration: Snackbar.LENGTH_SHORT,
+            });
+          });
+      })
+      .catch((err) => {
         Snackbar.show({
-          title: 'Location Updated',
-          duration: Snackbar.LENGTH_SHORT,
-        });
-
-        this.setState(
-          {
-            current: {
-              title: '',
-              index: 0,
-            },
-            locations: newLocations,
-          },
-        );
-      }).catch((err) => {
-        Snackbar.show({
-          title: 'Error in Updating Location',
+          title: 'Error in Converting Location to lat and lng',
           duration: Snackbar.LENGTH_SHORT,
         });
       });
-    }).catch((err) => {
-      Snackbar.show({
-        title: 'Error in Converting Location to lat and lng',
-        duration: Snackbar.LENGTH_SHORT,
-      });
-    });
   }
 
   addLocation() {
     const address = this.state.newLoc.description;
-    Geocode.fromAddress(address).then((res) => {
-      const { lat, lng } = res.results[0].geometry.location;
-      const markerRef = firebase.database().ref('locationMap/markers');
+    Geocode.fromAddress(address)
+      .then((res) => {
+        const { lat, lng } = res.results[0].geometry.location;
+        const markerRef = firebase.database().ref('locationMap/markers');
 
-      const storageRef = firebase.storage().ref('');
+        // const storageRef = firebase.storage().ref('');
 
-      console.log('got location');
+        if (
+          this.state.locations.find(
+            (loc) => loc.title === this.state.newLoc.title,
+          ) !== undefined
+        ) {
+          Snackbar.show({
+            title: 'A Location Already Has That Name',
+            duration: Snackbar.LENGTH_SHORT,
+          });
+          return;
+        }
 
-      const newLocations = [...this.state.locations];
-      newLocations.push({
-        coordinate: { latitude: lat, longitude: lng },
-        description: this.state.newLoc.description,
-        title: this.state.newLoc.title,
-      });
-
-      console.log('added location');
-
-      markerRef.set(newLocations).then(() => {
-        this.setState(
-          {
-            current: {
-              title: '',
-              index: 0,
-            },
-            locations: newLocations,
-          },
-        );
-
-        Snackbar.show({
-          title: 'Location Added',
-          duration: Snackbar.LENGTH_SHORT,
+        const newLocations = [...this.state.locations];
+        newLocations.push({
+          coordinate: { latitude: lat, longitude: lng },
+          description: this.state.newLoc.description,
+          title: this.state.newLoc.title,
         });
-      }).catch((err) => {
+
+        markerRef
+          .set(newLocations)
+          .then(() => {
+            this.setState({
+              current: {
+                title: '',
+                index: 0,
+              },
+              locations: newLocations,
+            });
+
+            Snackbar.show({
+              title: 'Location Added',
+              duration: Snackbar.LENGTH_SHORT,
+            });
+          })
+          .catch((err) => {
+            Snackbar.show({
+              title: 'Error in Adding Location',
+              duration: Snackbar.LENGTH_SHORT,
+            });
+          });
+      })
+      .catch((err) => {
         Snackbar.show({
-          title: 'Error in Adding Location',
-          duration: Snackbar.LENGTH_SHORT,
+          title: 'Error in Converting Location to lat and lng',
+          duration: Snackbar.LENGTH_INDEFINITE,
         });
       });
-    }).catch((err) => {
-      Snackbar.show({
-        title: 'Error in Converting Location to lat and lng',
-        duration: Snackbar.LENGTH_INDEFINITE,
-      });
-    });
   }
 
   deleteLocation() {
     const currLoc = this.state.current;
-    console.log('setting state');
 
     const newLocations = [...this.state.locations];
     newLocations.splice(currLoc.index, 1);
-    console.log('set state');
+
+    this.setState({
+      current: {
+        title: '',
+        index: 0,
+      },
+      locations: newLocations,
+    });
 
     const markerRef = firebase.database().ref('locationMap/markers');
-    console.log(`got database ${markerRef}`);
-    markerRef.set(newLocations).then(() => {
-      console.log('deleted');
-      Snackbar.show({
-        title: 'Location Deleted',
-        duration: Snackbar.LENGTH_SHORT,
+
+    markerRef
+      .set(newLocations)
+      .then(() => {
+        console.log('deleted');
+        Snackbar.show({
+          title: 'Location Deleted',
+          duration: Snackbar.LENGTH_SHORT,
+        }); 
+      })
+      .catch((err) => {
+        Snackbar.show({
+          title: 'Error in Deleting Location',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+
+        const newLocs = [...this.state.locations].push(cu);
       });
-      this.setState(
-        {
-          current: {
-            title: '',
-            index: 0,
-          },
-          locations: newLocations,
-        },
-      );
-    }).catch((err) => {
-      Snackbar.show({
-        title: 'Error in Deleting Location',
-        duration: Snackbar.LENGTH_SHORT,
-      });
-    });
   }
 
   componentDidMount() {
     Geocode.setApiKey(secrets.google);
     const ref = firebase.database().ref('locationMap');
     ref.on('value', (snapshot) => {
-      this.setState({ locations: snapshot.val().markers }); });
+      this.setState({ locations: snapshot.val().markers });
+    });
   }
 
   render() {
     return (
-        <ScrollView contentContainerStyle={{
-          backgroundColor: '#f6f6f6', flexGrow: 1, justifyContent: 'flex-start', alignItems: 'stretch', paddingBottom: 20,
+      <ScrollView
+        contentContainerStyle={{
+          backgroundColor: '#f6f6f6',
+          flexGrow: 1,
+          justifyContent: 'flex-start',
+          alignItems: 'stretch',
+          paddingBottom: 20,
         }}>
-            <Text style={styles.requestTitle}>{localization.editLoc}</Text>
-            <Picker
-              style={{ width: '80%' }}
-              selectedValue={this.state.current.title}
-              onValueChange={(itemValue, itemIndex) => {
-                this.setState({ current: { title: itemValue, index: itemIndex - 1 } });
-              }}
-            >
-              <Picker.Item label={ localization.selectLoc } value={{
-                title: 'default',
-                index: -1,
-              }} />
-              {this.state.locations != null ? this.state.locations.map((location, index) => {
-                const picker = (
-                  <Picker.Item key={index} label={location.title} value={location.title}/>
-                );
-
-                // console.debug(`${location.title}\n ${index}\n ${this.state.locations.length}`);
-
-                return picker;
-              }): null}
-            </Picker>
-
-            <Text style={styles.requestTitle}>
-              {localization.enterWait}
-            </Text>
-
-            <Input
-              style={myStyles.input}
-              placeholder={localization.waitHint}
-              autoCapitalize="none"
-              defaultValue={this.state.wait}
-              onChangeText={(wait) => this.setState({ wait })}
-            />
-
-          <Touchable
-            onPress={() => {
-              const today = new Date();
-              const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-              const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-              const dateTime = `${date} ${time}`;
-              this.state.current.index = this.state.current.index - 1;
-              firebase.database().ref(`locationMap/markers/${this.state.current.index}/`).update({
-                waitTime: this.state.wait,
-                lastUpdated: dateTime,
-              });
+        <Text style={styles.requestTitle}>{localization.editLoc}</Text>
+        <Picker
+          style={{ width: '80%' }}
+          selectedValue={this.state.current.title}
+          onValueChange={(itemValue, itemIndex) => {
+            this.setState({ current: { title: itemValue, index: itemIndex - 1 } });
+          }}>
+          <Picker.Item
+            label={localization.selectLoc}
+            value={{
+              title: 'default',
+              index: -1,
             }}
-            style={styles.submitButton}
-          >
+          />
+          {this.state.locations != null
+            ? this.state.locations.map((location, index) => {
+              const picker = (
+                  <Picker.Item
+                    key={index}
+                    label={location.title}
+                    value={location.title}
+                  />
+              );
+
+              return picker;
+            })
+            : null}
+        </Picker>
+
+        <Text style={styles.requestTitle}>{localization.enterWait}</Text>
+
+        <Input
+          style={myStyles.input}
+          placeholder={localization.waitHint}
+          autoCapitalize="none"
+          defaultValue={this.state.current.waitTime}
+          onChangeText={(waitTime) => {
+            const newCurr = this.state.current;
+            newCurr.waitTime = waitTime;
+            this.setState({ newCurr });
+          }}
+        />
+
+        <Touchable
+          onPress={() => {
+            const today = new Date();
+            const date = `${today.getFullYear()}-${today.getMonth()
+              + 1}-${today.getDate()}`;
+            const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+            const dateTime = `${date} ${time}`;
+            // this.state.current.index = this.state.current.index - 1;
+            firebase
+              .database()
+              .ref(`locationMap/markers/${this.state.current.index}/`)
+              .update({
+                waitTime: this.state.current.waitTime,
+                lastUpdated: dateTime,
+              }).then(() => {
+                Snackbar.show({
+                  title: 'Wait Time Updated',
+                  duration: Snackbar.LENGTH_SHORT,
+                });
+              })
+              .catch(() => {
+                Snackbar.show({
+                  title: 'Error in Updating Wait Time',
+                  duration: Snackbar.LENGTH_SHORT,
+                });
+              });
+          }}
+          style={styles.submitButton}>
+          <View>
+            <Text style={styles.cardtext}>{localization.timePrompt}</Text>
+          </View>
+        </Touchable>
+
+        <View style={myStyles.editTools}>
+          <Input
+            placeholder={localization.locNameHint}
+            defaultValue={
+              this.state.locations != null
+              && this.state.locations[this.state.current.index] != null
+              && this.state.current.title !== ''
+                ? this.state.locations[this.state.current.index].title
+                : ''
+            }
+            onChangeText={(title) => {
+              if (this.state.newLoc.description !== '') {
+                this.setState({
+                  newLoc: { title, description: this.state.newLoc.description },
+                });
+              } else {
+                this.setState({
+                  newLoc: { title, description: this.state.current.description },
+                });
+              }
+            }}
+          />
+          <Input
+            placeholder={localization.locAddHint}
+            defaultValue={
+              this.state.locations != null
+              && this.state.locations[this.state.current.index] != null
+              && this.state.current.title !== ''
+                ? this.state.locations[this.state.current.index].description
+                : ''
+            }
+            onChangeText={(description) => {
+              if (this.state.newLoc.title !== '') {
+                this.setState({
+                  newLoc: { title: this.state.newLoc.title, description },
+                });
+              } else {
+                this.setState({
+                  newLoc: { title: this.state.current.title, description },
+                });
+              }
+            }}
+          />
+          <Touchable
+            style={myStyles.submitButton}
+            onPress={this.updateLocation}>
             <View>
-                <Text style={styles.cardtext}>
-                  {localization.timePrompt}
-                </Text>
+              <Text style={styles.cardtext}>{localization.locPrompt}</Text>
             </View>
           </Touchable>
-
-          <View style={myStyles.editTools}>
-            <Input
-              placeholder={localization.locNameHint}
-              defaultValue={((this.state.locations != null && this.state.locations[this.state.current.index] != null && this.state.current.title !== '')
-                ? this.state.locations[this.state.current.index].title
-                : '')}
-              onChangeText={(title) => {
-                if (this.state.newLoc.description !== '') {
-                  this.setState({ newLoc: { title, description: this.state.newLoc.description } });
-                } else {
-                  this.setState({ newLoc: { title, description: this.state.current.description } });
-                }
-              }}
-            />
-            <Input
-              placeholder={localization.locAddHint}
-              defaultValue={ ((this.state.locations != null && this.state.locations[this.state.current.index] != null && this.state.current.title !== '')
-                ? this.state.locations[this.state.current.index].description
-                : '')
-              }
-              onChangeText={(description) => {
-                if (this.state.newLoc.title !== '') {
-                  this.setState({ newLoc: { title: this.state.newLoc.title, description } });
-                } else {
-                  this.setState({ newLoc: { title: this.state.current.title, description } });
-                }
-              }}
-            />
-            <Touchable style={myStyles.submitButton} onPress={ this.updateLocation }>
-              <View>
-                <Text style={styles.cardtext}>
-                  {localization.locPrompt}
-                </Text>
-              </View>
-            </Touchable>
-            <Touchable style={myStyles.submitButton} onPress={ () => this.addLocation() }>
-              <View>
-                <Text style={styles.cardtext}>
-                  {localization.addPrompt}
-                </Text>
-              </View>
-            </Touchable>
-            <Touchable style={myStyles.submitButton} onPress={ () => this.deleteLocation() }>
-              <View>
-                <Text style={styles.cardtext}>
-                  {localization.deletePrompt}
-                </Text>
-              </View>
-            </Touchable>
-          </View>
-        </ScrollView>
+          <Touchable
+            style={myStyles.submitButton}
+            onPress={() => this.addLocation()}>
+            <View>
+              <Text style={styles.cardtext}>{localization.addPrompt}</Text>
+            </View>
+          </Touchable>
+          <Touchable
+            style={myStyles.submitButton}
+            onPress={() => this.deleteLocation()}>
+            <View>
+              <Text style={styles.cardtext}>{localization.deletePrompt}</Text>
+            </View>
+          </Touchable>
+        </View>
+      </ScrollView>
     );
   }
 }
