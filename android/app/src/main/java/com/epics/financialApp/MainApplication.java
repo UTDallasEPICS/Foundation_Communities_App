a1d4e39ff41968bdfe54a88189b94bc80d93cc8d
Frankie Flores <floresfrankie003@gmail.com>
2020-04-28 21:37:16 -0500

    Post notification works, but there is an error.

--- android/app/src/main/java/com/epics/financialApp/MainApplication.java
+++ android/app/src/main/java/com/epics/financialApp/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.imagepicker.ImagePickerPackage;
 import com.azendoo.reactnativesnackbar.SnackbarPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
@@ -15,11 +16,14 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.shell.MainReactPackage;
 
 import java.util.List;
 
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
-import com.airbnb.android.react.maps.MapsPackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -31,12 +35,18 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
+
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
+
+      //packages.add(new MainReactPackage(),
       packages.add(new RNFirebaseDatabasePackage());
-      // packages.add(new MapsPackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
+
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+
       return packages;
     }
 
